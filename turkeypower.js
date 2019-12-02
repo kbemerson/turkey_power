b0c8dab39c9bde37fa9de7aa558e7150e4febe1e
Katie Emerson <kbemerson27@gmail.com>
2019-12-02 09:58:59 -0700

    1

---
+++ turkeypower.js
@@ -0,0 +1,14 @@
+function ready() {
+
+    console.log("Page Ready");
+    turkey.onclick = function() {
+        for (i = 0; i < 1000; i * 2) {
+        var i;
+        }
+        for (i = 1000; i >= 0; i = i - 1) {
+            document.write("https://fulfillnj.org/wp-content/uploads/2017/11/cartoon-turkey.png");
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
